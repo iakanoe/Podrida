c18aacb77569ed54d4cbbd964bb7e11371bb7529
iakanoe <iakanoe1056@gmail.com>
2017-10-01 17:19:19 -0300

    First commit

---
+++ app/src/main/java/com/winoe/podrida/configActivity.java
@@ -0,0 +1,10 @@
+package com.winoe.podrida;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class configActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+}
